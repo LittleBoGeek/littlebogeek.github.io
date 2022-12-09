4281d006a71865dad4616b6b4903282ac8e854ec
Nadia <NadiaChilds@outlook.com>
2022-12-09 01:16:24 -0500

    update remix config and gitignore

--- remix.config.js
+++ remix.config.js
@@ -1,1 +1,6 @@
-module.exports = {};
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  appDirectory: "./app",
+  ignoredRouteFiles: ["**/.*"],
+  assetsBuildDirectory: "public/build",
+};
